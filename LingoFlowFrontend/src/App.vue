af1c662e1c5e3866c56562b5d667a4766c521e9a
彭涛 <1254958718@qq.com>
2026-04-09 09:13:00 +0800

    1

---
+++ LingoFlowFrontend/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <router-view></router-view>
+</template>
+
+<style>
+/* 全局锁死滚动条，重置样式 */
+html, body, #app {
+  margin: 0; padding: 0; width: 100%; height: 100%;
+  overflow: hidden; 
+  box-sizing: border-box;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, sans-serif;
+}
+* { box-sizing: inherit; }
+</style>
